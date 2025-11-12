b08d77daba08072cfc8e728ef8d90e724229ce92
SHussain47 <hussain.syed.6732@gmail.com>
2025-11-12 16:28:04 -0500

    feat: Added error handler | Added /user routers

--- api/users.js
+++ api/users.js
@@ -2,6 +2,23 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { createUser } from "#db/queries/users";
+import { createUser, getUserByUsernamePassword } from "#db/queries/users";
+import requireBody from "#middleware/requireBody";
+import { createToken } from "#utils/jwt";
 
+router.post("/register", requireBody(["username", "password"]), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await createUser(username, password);
+  const token = createToken({ id: user.id });
+  res.status(201).send(token);
+});
 
+router.post("/login", requireBody(["username", "password"]), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await getUserByUsernamePassword(username, password);
+
+  if (!user) return res.status(401).send("Invalid Username OR Password");
+
+  const token = createToken({ id: user.id });
+  res.send(token);
+});
--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 import express from "express";
 const app = express();
 export default app;
+
+import morgan from "morgan";
+import getUserFromToken from "#middleware/getUserFromToken";
+import usersRouter from "#api/users";
+
+// JSON Parser
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(getUserFromToken);
+
+// Paths
+app.use("/users", usersRouter);
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  switch (error.code) {
+    case "22P02":
+      return res.status(400).json({ error: error.message });
+    case "23505":
+    case "23503":
+      return res.status(400).json({ error: error.detail });
+    default:
+      return next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({ error: "Something went wrong!" });
+});
